785429175acab350e59e7d1fd97d143a547bfde8
schazbot <charlotte.pearce1984@gmail.com>
2019-05-27 12:58:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num1 = 31 
+let num2 = 2
+const multiply = num1 * num2
+
+const mod = 40 % 6;
+
+const max = Math.max(1, 2, 20)
+
